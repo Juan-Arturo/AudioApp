c33b4efc02c4284d9e133f439ea8332462d0356c
Juan-Arturo <jagpjagp002@gmail.com>
2024-02-11 12:17:47 -0600

    (generated by blackbox.ai )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './main/home/home.component';
+import { ServiceComponent } from './main/service/service.component';
+import { ProductsComponent } from './main/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ServiceComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,17 @@
+import { Component,  AfterViewInit, OnInit} from '@angular/core';
+
+
+
+
+
+@Component({
+  selector: 'appMain',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent {
+  constructor() { }
+
+ 
+ 
+}
